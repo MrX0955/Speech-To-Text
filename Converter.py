bbd61a7ca4a8b269a94674c842819bd66bc50398
Empty <77683874+MrX0955@users.noreply.github.com>
2024-01-21 18:25:35 +0300

    Update Converter.py

--- Converter.py
+++ Converter.py
@@ -1,23 +1,27 @@
 import speech_recognition as sr
 import os
-from pydub  import AudioSegment
+from pydub import AudioSegment
+
+def clear(): os.system('cls' if os.name == 'nt' else 'clear')
+
 
 def mp3towav():
-    os.system('cls')
+    clear()
     src = input("Enter your mp3 file $PATH or directly name: ")
     dst = "output.wav"
-    
+
     sound = AudioSegment.from_mp3(src)
     sound.export(dst, format="wav")
 
 
 def main():
-    mp3towav()    
+    mp3towav()
     r = sr.Recognizer()
-    
+
     with sr.AudioFile("output.wav") as cleinkelvinn:
         audio_data = r.record(cleinkelvinn)
         text = r.recognize_google(audio_data)
-        print("\n=============???=============\n"+text+"\n=============???=============")
+        print("\n=============???=============\n" + text + "\n=============???=============")
+
 
 main()
